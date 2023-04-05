bcec4efed638f1d293853706ec0d96be3136809e
hy <479797503@qq.com>
2023-04-05 21:20:50 +0800

    1

---
+++ ivew-admin/src/view/home/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ThreeJS></ThreeJS>
+  </div>
+</template>
+
+<script>
+  import ThreeJS from "@/components/threejs/index";
+  export default {
+    name: "Index",
+    components:{
+      ThreeJS
+    },
+    data() {
+      return {
+        composer:'',
+        // 版本号
+        version: "3.8.3",
+      };
+    },
+    methods: {},
+    }
+</script>
+
+<style>
+</style>
